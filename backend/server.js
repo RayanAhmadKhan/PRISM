b533e7edb2b16b09589a4749d49b4fea7b601256
Ghulam Dastgir <ghulamdastgir12905@gmail.com>
2026-03-17 02:19:49 +0500

    Test API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import env from 'dotenv';
 import connectdb from './config/db.js';
+import testRoutes from './routes/testRoutes.js';
 env.config();
 const port = process.env.PORT || 3000;
 const app= express();
+
+app.use(express.json());
+app.use('/test', testRoutes);
 
 app.get('/',(req,res)=>{
   res.send("Hello World");
