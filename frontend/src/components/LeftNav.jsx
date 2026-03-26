aeb953d57885676014b9a47ac5ce44f38cefdd34
Areeb Ahmed <areebahmed.one@gmail.com>
2026-03-26 16:05:24 +0500

    used link tag from react-router-dom in left-nav

--- frontend/src/components/LeftNav.jsx
+++ frontend/src/components/LeftNav.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const LeftNav = (props) => {
     return (
         <div className="left-nav w-[15%] flex items-center justify-start py-6 gap-5 flex-col bg-zinc-900">
-            <button className='bg-blue-700 p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn1}</button>
-            <button className='bg-blue-700 p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn2}</button>
-            <button className='bg-blue-700 p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn3}</button>
-            <button className='bg-blue-700 p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn4}</button>
+            <Link className='bg-blue-700 flex justify-center p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn1}</Link>
+            <Link className='bg-blue-700 flex justify-center p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn2}</Link>
+            <Link className='bg-blue-700 flex justify-center p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn3}</Link>
+            <Link className='bg-blue-700 flex justify-center p-1 w-40 font-bold rounded-sm cursor-pointer hover:bg-blue-900'>{props.btn4}</Link>
         </div>
     )
 }
