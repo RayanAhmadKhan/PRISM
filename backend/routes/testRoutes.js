a9a415d92955317decb0acec5d09efa44e43df4b
Areeb Ahmed <areebahmed.one@gmail.com>
2026-03-28 18:15:26 +0500

    fixed /test get call in server.js

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -3,6 +3,10 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.json({message: "Test route working"});
+});
+
 router.post("/", testController);
 
 export default router;
