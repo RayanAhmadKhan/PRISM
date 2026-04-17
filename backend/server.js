8499de301ec2718011c35b15dbafc16b75a6fdc8
Ghulam Dastgir <ghulamdastgir12905@gmail.com>
2026-04-17 12:13:52 +0500

    Delete User API

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import env from 'dotenv';
+import express from "express";
+import env from "dotenv";
+import cors from "cors";
+import connectdb from "./config/db.js";
+import testRoutes from "./routes/testRoutes.js";
+import addUserRoutes from "./routes/addUserRoutes.js";
+import deleteUserRoutes from "./routes/deleteUserRouter.js";
+
 env.config();
 
-import cors from 'cors';
-
-import connectdb from './config/db.js';
-import testRoutes from './routes/testRoutes.js';
-import addUserRoutes from './routes/addUserRoutes.js'
-
 const port = process.env.PORT || 3000;
-const app= express();
+const app = express();
 
 app.use(cors());
 
 app.use(express.json());
-app.use('/test', testRoutes);
-app.use('/addUser', addUserRoutes)
+app.use("/test", testRoutes);
+app.use("/addUser", addUserRoutes);
+app.use("/deleteUser", deleteUserRoutes);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello World from Backend!" });
 });
 
@@ -25,7 +26,7 @@ console.log("Connecting to the database...");
 connectdb();
 console.log("MONGO_URI =", process.env.URI);
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log("Server is running");
-  console.log("http://localhost:"+port);
+  console.log("http://localhost:" + port);
 });
