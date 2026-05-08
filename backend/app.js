a356bc8f158da20bb1caeabd15cfccc18e27af2c
Ghulam Dastgir <ghulamdastgir12905@gmail.com>
2026-05-09 03:53:28 +0500

    fix app.js

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://prism-chi-indol.vercel.app/'
+    'https://prism-chi-indol.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
