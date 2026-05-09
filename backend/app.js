37f41200e0fcc816509c818cb6d9bd15662c8442
Ghulam Dastgir <ghulamdastgir12905@gmail.com>
2026-05-09 20:52:47 +0500

    Update frontend URL

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://prism-eta-seven.vercel.app'
+    'https://prism-smoky-ten.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
